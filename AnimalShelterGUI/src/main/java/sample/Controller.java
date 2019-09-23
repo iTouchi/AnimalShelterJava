395c426fcf5b627ac51d5ee67110a4dc531496ce
iTouchi <aytacsarkmaz@gmail.com>
2019-09-23 23:06:45 +0200

    Adding Cat's to List

--- AnimalShelterGUI/src/main/java/sample/Controller.java
+++ AnimalShelterGUI/src/main/java/sample/Controller.java
@@ -1,7 +1,8 @@
 package sample;
 
-import Classes.Cat;
+import Classes.*;
 import Classes.Gender;
+import Classes.Reservation;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -9,28 +10,76 @@ import javafx.scene.control.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
 
 
     public Button btnAddAnimal;
     public ComboBox cbSpecies;
     public TextField tbName;
+    public TextField tbBadHabits;
     public Label labelGender;
     public ListView lbAnimals;
+    public RadioButton rbMale;
+    public RadioButton rbFemale;
 
 
-    public Cat simboe = new Cat("Simba", Gender.MALE,"Bites");
+    public Reservation reservation = new Reservation();
+
+
+    public String name;
+    public Gender gender;
+    public String badHabits;
+    public Cat cat;
+    public Dog dog;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
-    public void onClick(ActionEvent actionEvent) {
+    public void addAnimalOnClick(ActionEvent actionEvent) {
 
-        String name = tbName.textProperty().get();
-        labelGender.textProperty().set(name);
-        lbAnimals.getItems().add(simboe);
+        String species = cbSpecies.getSelectionModel().getSelectedItem().toString();
+
+        name = tbName.textProperty().get();
+        badHabits = tbBadHabits.textProperty().get();
+
+//        lbAnimals.getItems().add(simboe);
+
+        if (species.equals("Cat")) {
+//            cat.setBadHabits(badHabits);
+//            cat.setGender(gender);
+//            cat.setName(name);
+            reservation.newCat(name, gender, badHabits);
+
+        } else if (species.equals("Dog")) {
+            dog.setGender(gender);
+            dog.setName(name);
+            reservation.Animals.add(dog);
+        } else {
+            labelGender.textProperty().set("Select Species");
+        }
+
+        lbAnimals.getItems().clear();
+        reservation.Animals.forEach((n) ->  lbAnimals.getItems().add(n));
+
+
+
+    }
+
+    public void RadioButtonFemaleOnClick(ActionEvent actionEvent) {
+        rbMale.setSelected(false);
+        rbFemale.setSelected(true);
+        gender = Gender.FEMALE;
+    }
+
+    public void RadioButtonMaleOnClick(ActionEvent actionEvent) {
+        rbMale.setSelected(true);
+        rbFemale.setSelected(false);
+        gender = Gender.MALE;
+    }
+
+    public void onClickAddReservor(ActionEvent actionEvent) {
     }
 }
 
