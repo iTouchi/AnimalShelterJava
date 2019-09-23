1bd6154756f15ad3abcf0ce79bb5a0adcc46d52e
iTouchi <aytacsarkmaz@gmail.com>
2019-09-23 18:12:08 +0200

    GUI and Core projects are connected

--- AnimalShelterGUI/src/main/java/sample/Controller.java
+++ AnimalShelterGUI/src/main/java/sample/Controller.java
@@ -2,13 +2,21 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller implements Initializable{
 
 
+    public Button btnAddAnimal;
+    public ComboBox cbSpecies;
+    public TextField tbName;
+    public Label label1;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -16,6 +24,10 @@ public class Controller implements Initializable {
     }
 
     public void onClick(ActionEvent actionEvent) {
+
+        String name = tbName.textProperty().get();
+        label1.textProperty().set(name);
+
     }
 }
 
