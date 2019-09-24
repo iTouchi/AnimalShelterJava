1203e1d5e8a4d6904cb33e136fa8e073c208308e
iTouchi <aytacsarkmaz@gmail.com>
2019-09-24 21:40:55 +0200

    Jenkins not working test

--- AnimalShelterCore/src/main/java/module-info.java
+++ AnimalShelterCore/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module AnimalShelterCore {
+
     exports Classes;
 }
--- AnimalShelterCore/src/test/java/CatTest.java
+++ AnimalShelterCore/src/test/java/CatTest.java
@@ -1,4 +1,5 @@
 import Classes.*;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
