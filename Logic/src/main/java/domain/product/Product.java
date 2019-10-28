29fa3e8327e14cfc45e40dc8e9d3d29c04ded1a4
iTouchi <aytacsarkmaz@gmail.com>
2019-10-28 22:20:43 +0100

    Websockets

--- Logic/src/main/java/domain/product/Product.java
+++ Logic/src/main/java/domain/product/Product.java
@@ -1,39 +1,5 @@
 package domain.product;
 
-//public class Product {
-//
-//    private String name;
-//    private Double price;
-//
-//    public Product() {
-//    }
-//
-//    public Product(String name, Double price){
-//        this.name = name;
-//        this.price = price;
-//    }
-//
-//    public String toString(){
-//        return "Product: " + this.name + ", " + "$ " + this.price;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getName(){
-//        return this.name;
-//    }
-//
-//    public void setPrice(Double price) {
-//        this.price = price;
-//    }
-//
-//    public Double getPrice(){
-//        return this.price;
-//    }
-//}
-
 import domain.ISellable;
 
 public class Product implements ISellable {
